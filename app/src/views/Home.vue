4a5d0177f807c36348243fd9b1f03bf1c4562a37
Felix Baron <45421716+felixbaron@users.noreply.github.com>
2021-01-07 21:51:55 +0100

    add vuetify app

---
+++ app/src/views/Home.vue
@@ -0,0 +1,16 @@
+<template>
+    <!-- <HelloWorld msg="Welcome to Your Vue.js App"/> -->
+  <div>Home</div>
+</template>
+
+<script>
+// @ is an alias to /src
+// import HelloWorld from '@/components/HelloWorld.vue'
+
+// export default {
+//   name: 'home',
+//   components: {
+//     helloworld
+//   }
+// }
+</script>
